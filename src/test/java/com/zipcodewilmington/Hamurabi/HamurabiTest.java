fdde3942571f50b69a2243e8eff800d74e340ec0
DeeptiMishra@1512 <mishra.deeptimishra@gmail.com>
2024-11-10 15:16:24 -0500

    commit1

---
+++ src/test/java/com/zipcodewilmington/Hamurabi/HamurabiTest.java
@@ -0,0 +1,38 @@
+package com.zipcodewilmington.Hamurabi;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class HamurabiTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public HamurabiTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( HamurabiTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
